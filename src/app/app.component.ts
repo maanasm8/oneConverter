33d4d0144abd60d254e3d2a56d62da24370e6c28
maanasm8 <maanasm8@vt.edu>
2022-05-19 08:35:12 -0400

    commented property decorators
    commented out @Input and @Output property decorators for now in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
+//@Component and @NgModule are class decorators, carry meta data
+//This @Component carries selector, templateURL, styleURL meta data, style and template are optional.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +9,7 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class AppComponent {
   title = 'oneConverter';
-  @Input() data: string;
-  @Output() params: string;
+  //@Input and @Output are parameter/property decorators
+  // @Input() data: string;
+  // @Output() params: string;
 }
